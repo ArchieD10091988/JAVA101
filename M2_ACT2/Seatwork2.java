0d4214c1434971a848bc10e2b945f1466711ef76
Archie O. Datuin <aodatuin@headoffice.corp.bpi.com.ph>
2025-12-09 16:29:28 +0800

    commit m2 act 2

---
+++ M2_ACT2/Seatwork2.java
@@ -0,0 +1,17 @@
+package module2.seatwork2;
+
+public class Seatwork2 {
+
+public static void main(String[] args) {
+
+	WhiteBoard wb = new WhiteBoard();
+	int sum = wb.addNumbers(5, 3);
+	
+	System.out.println("The total is: " + String.valueOf(sum));
+	
+	
+}
+
+}
+
+
